9f8def1e5d2cc88899f4ae1bc3637a2217a14975
Abhinav Kumar <kabhinav713@gmail.com>
2023-06-22 11:23:07 +0530

    [Abhinav]-adding HappyOrSad

---
+++ MoodAanalyser1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser2
+{
+    public class MoodAanalyser1
+    {
+        public string HAPPYMOOD = "Happy";
+        public string SADMOOD = "Sad";
+        public string AnalyseMood(string message)
+        {
+            if (message.ToUpper().Contains("SAD"))
+            {
+                return "Sad Mood";
+            }
+            else
+            {
+                return "Happy Mood";
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using MoodAnalyser2;
+
+Console.WriteLine("Welcome to Mood Analyser Program");
+MoodAanalyser1 moodobj = new MoodAanalyser1();
+
+Console.WriteLine("Enter your Mood:");
+string message = Console.ReadLine();
+
+Console.WriteLine(moodobj.AnalyseMood(message));
